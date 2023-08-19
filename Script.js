aa5777ae89769cb2fbfac2c285f92779dab66e96
Sukanta <sukantabhun@gmail.com>
2023-08-19 21:41:21 +0530

    placeholder

--- Script.js
+++ Script.js
@@ -1,18 +1,19 @@
 let temp = true;
 let graphEl = document.getElementById('plot');
+let placeHolderEl = document.getElementById('plotPlaceholder');
 graphEl.classList.add('d-none')
 
 
 function predictToggle() {
     let graphEl = document.getElementById('plot');
-    
+    let placeHolderEl = document.getElementById('plotPlaceholder');
     if (temp === false) {
         graphEl.classList.remove('d-none');
+        placeHolderEl.classList.add('d-none');
         temp = true;
     } else {
         graphEl.classList.add('d-none');
+        placeHolderEl.classList.remove('d-none');
         temp = false;
     }
 }
-
-
